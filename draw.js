6af8216e5ec08068df7252b4abc08ce944a7a503
Richard Hansen <rhansen@rhansen.org>
2022-02-03 01:31:29 -0500

    Move settings processing to `loadSettings` hook

--- draw.js
+++ draw.js
@@ -7,23 +7,19 @@ exports.eejsBlock_editbarMenuLeft = (hookName, context) => {
   context.content += eejs.require('ep_whiteboard/templates/editbarButtons.ejs');
 };
 
-exports.clientVars = async (hookName, context) => {
-  if (!settings.host) {
+exports.clientVars = async (hookName, context) => ({ep_whiteboard: settings});
+
+exports.loadSettings = async (hookName, {settings: {ep_draw: pluginSettings = {}}}) => {
+  if (!pluginSettings.host) {
     console.warn(
         'ep_draw.host NOT SET in settings.json. This must be set to the host name (optionally ' +
         'followed by port and base path) to the WBO server. For example: ' +
         '"ep_draw" {"host": "etherpad.example.com:5001/wbo"}');
   }
-  return {
-    ep_whiteboard: Object.assign({
-      host: 'draw.etherpad.org',
-      onByDefault: false,
-      icon: '../static/plugins/ep_whiteboard/static/img/icon.png',
-      position: 'left',
-    }, settings),
-  };
-};
-
-exports.loadSettings = async (hookName, {settings: {ep_draw: pluginSettings = {}}}) => {
-  settings = pluginSettings;
+  settings = Object.assign({
+    host: 'draw.etherpad.org',
+    onByDefault: false,
+    icon: '../static/plugins/ep_whiteboard/static/img/icon.png',
+    position: 'left',
+  }, pluginSettings);
 };
